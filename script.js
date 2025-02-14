2da1a1d39fbd51e2a435bd3a1aa25e4789478842
zartul <121471819+zartul@users.noreply.github.com>
2025-02-14 12:31:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
+getSumBtn.id="ans";
 document.body.appendChild(getSumBtn);
-
-
 
 const getSum = () => {
 
     const priceElems = document.querySelectorAll(".price");
-
+	let ansElem = document.querySelector("#ans");
     let total = 0;
-    priceElems.forEach(price => {
-        total += parseFloat(price.textContent);
-    });
+    for (let i = 0; i < priceElems.length; i++) {
+	    total += parseFloat(priceElems[i].textContent);
+	}
 
     let table = document.querySelector("table");
     let totalPriceRow = table.insertRow(-1);
     let totalPriceCell = totalPriceRow.insertCell(0);
     totalPriceCell.textContent = `Total Price: Rs ${total}`;
-   
 };
 
 getSumBtn.addEventListener("click", getSum);
